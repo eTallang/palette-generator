74813e4f60949d5728840b222bdcf2fcc5db06df
Erik Tallang <e.tallang@gmail.com>
2019-08-11 13:32:44 +0200

    feat(color): support complementary color scheme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,17 +17,49 @@ export class AppComponent {
     { hue: 347, saturation: 54, lightness: 13 }
   ];
   mode: ShadeMode = 'monochromatic';
+  numberOfTrueColors = Math.round(this.colors.length / 2);
 
   generatePalette(): void {
+    /**
+     * TODO: Select hue from a wheel
+     */
     const hue = this.randomHue();
-    this.colors.forEach(color => this.updateColor(color, hue, this.mode));
+    this.colors.forEach((color, index) => this.updateColor(color, index, hue, this.mode));
   }
 
-  private updateColor(color: HslColor, hue: number, mode: ShadeMode): void {
-    if (mode === 'monochromatic') {
-      color.hue = hue;
-      color.saturation = this.randomPercent();
-      color.lightness = this.randomPercent();
+  private updateColor(color: HslColor, index: number, hue: number, mode: ShadeMode): void {
+    switch (mode) {
+      case 'monochromatic': {
+        /**
+         * TODO
+         *   - Spread hue based on a factor
+         *   - Do not do complete random saturation and lightness.
+         *     1. Strong saturation, primary
+         *     2. Lighter version of primary
+         *     3. Gray scale color
+         *     4. Close to white
+         *     5. Close to black
+         */
+        color.hue = hue;
+        color.saturation = this.randomPercent();
+        color.lightness = this.randomPercent();
+        break;
+      }
+      case 'triad': {
+        break;
+      }
+      case 'complementary': {
+        let oppositeHue: number;
+        if (hue < 180) {
+          oppositeHue = hue + 180;
+        } else {
+          oppositeHue = hue - 180;
+        }
+
+        color.hue = index + 1 > this.numberOfTrueColors ? hue : oppositeHue;
+        color.saturation = this.randomPercent();
+        color.lightness = this.randomPercent();
+      }
     }
   }
 
