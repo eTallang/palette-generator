21eb8b149d0dc43efec6e99b7db6f323a12df64c
Erik Tallang <e.tallang@gmail.com>
2019-08-11 12:48:23 +0200

    feat(color): support monochromatic spectrum

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { HslColor } from './hsl-color';
 
+type ShadeMode = 'monochromatic' | 'triad' | 'complementary';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -14,4 +16,26 @@ export class AppComponent {
     { hue: 230, lightness: 50, saturation: 100 },
     { hue: 230, lightness: 50, saturation: 100 }
   ];
+  mode: ShadeMode = 'monochromatic';
+
+  generatePalette(): void {
+    const hue = this.randomHue();
+    this.colors.forEach(color => this.updateColor(color, hue, this.mode));
+  }
+
+  private updateColor(color: HslColor, hue: number, mode: ShadeMode): void {
+    if (mode === 'monochromatic') {
+      color.hue = hue;
+      color.saturation = this.randomPercent();
+      color.lightness = this.randomPercent();
+    }
+  }
+
+  private randomHue(): number {
+    return Math.random() * 360;
+  }
+
+  private randomPercent(): number {
+    return Math.random() * 100;
+  }
 }
