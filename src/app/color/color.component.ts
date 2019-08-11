066bb2a5e845ff1d0dfa2df73ba6916f0f0e8a12
Erik Tallang <e.tallang@gmail.com>
2019-08-11 11:13:08 +0200

    feat(color): convert hsl to hex

--- src/app/color/color.component.ts
+++ src/app/color/color.component.ts
@@ -1,12 +1,19 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
+import * as convert from 'color-convert';
 
 @Component({
   selector: 'app-color',
   templateUrl: './color.component.html',
   styleUrls: ['./color.component.scss']
 })
-export class ColorComponent {
+export class ColorComponent implements OnChanges {
   @Input() h: number;
   @Input() s: number;
   @Input() l: number;
+
+  hex: string;
+
+  ngOnChanges() {
+    this.hex = convert.hsl.hex([this.h, this.s, this.l]);
+  }
 }
